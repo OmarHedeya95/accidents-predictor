ceba668706a9acd720857707c489b2ccb499ad7c
= <=>
2021-07-16 13:23:57 +0200

    Reorganizing the repo, removing comments

--- app.py
+++ app.py
@@ -28,5 +28,4 @@ def makecalc():
 if __name__ == '__main__':
     modelfile = 'models/regressor.joblib'
     model = joblib.load(modelfile)
-    #app.run(debug=True, host='0.0.0.0')
     app.run(host='0.0.0.0', port=8080)
